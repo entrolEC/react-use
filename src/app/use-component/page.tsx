7cad5f21df5abb29342f0a81763e383fad416991
entrolEC <euichan1108@gmail.com>
2025-05-11 16:21:49 +0900

    fix: use 페이지 수정

--- src/app/use-component/page.tsx
+++ src/app/use-component/page.tsx
@@ -1,27 +1,34 @@
 import { fetchProducts } from '@/libs/fetchProducts';
 import { Suspense, use } from 'react';
+import { Product } from '@/libs/types/types';
+
+export const dynamic = 'force-dynamic';
 
 export default function Page() {
+  const productsPromise = fetchProducts();
+
   return (
     <div>
       <h1>Use</h1>
       <Suspense fallback={<div>Loading...</div>}>
-        <Products />
+        <Products productsPromise={productsPromise} />
       </Suspense>
     </div>
   );
 }
 
-function Products() {
-  const products = use(fetchProducts());
+function Products({ productsPromise }: { productsPromise: Promise<Product[]> }) {
+  const products = use(productsPromise);
 
   return (
-    <ul className="flex flex-col gap-4">
-      {products.map((product) => (
-        <li key={product.id} className="border border-green-400">
-          <strong>{product.title}</strong>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="flex flex-col gap-4">
+        {products.map((product) => (
+          <li key={product.id} className="border border-green-400">
+            <strong>{product.title}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
